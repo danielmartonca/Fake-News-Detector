6956a44c7ad0257b9c32b07fea8be0e59cddca27
Martoncă Daniel <45790572+danielmartonca@users.noreply.github.com>
2022-01-11 09:20:11 +0200

    Fixed front-end testing

--- REST-API/ClientApp/public/tests/addTest.js
+++ REST-API/ClientApp/public/tests/addTest.js
@@ -1,19 +1,19 @@
-﻿QUnit.test("Basic Test", function (assert) => {
+﻿QUnit.test("Basic Test", function (assert) {
     var result = add(1, 2);
     assert.equal(result, 3);
 });
 
-QUnit.test("Web Test", function (assert) => {
+QUnit.test("Web Test", function (assert) {
     var result = isValidHttpUrl("https://www.google.com/");
-    assert.equal(result, "https");
+    assert.equal(result, true);
 });
 
-QUnit.test("Web Test2", function (assert) => {
-    var result = isValidHttpUrl("http://www.mmediu.ro/");
-        assert.equal(result, "http");
+QUnit.test("Web Test2", function (assert) {
+    var result = isValidHttpUrl("httpd://www.mmediu.ro/");
+    assert.equal(result, false);
 });
 
-QUnit.test("Web Test3", function (assert) => {
-    var result = getCookie("testtest");
-        assert.equal(result, "null");
+QUnit.test("Web Test3", function (assert) {
+    var result = getCookie("userLogged");
+    assert.equal(result, null);
 });
